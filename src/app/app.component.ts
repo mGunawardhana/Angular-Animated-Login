77123d7a8d2d896f6328804a39e2da8fc47b2e26
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-10-11 17:24:43 +0530

    content updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'my-angular-app';
-  my_name = 'My name is Maneesha!';
-  name = 'Maneesha Gunawardhana';
-  show: boolean = true;
-
-  public employee: any = [
-    { id: 1, name: 'Maneesha', salary: 120000, gender: 'male' },
-    { id: 2, name: 'Gawesha', salary: 120000, gender: 'male' },
-    { id: 3, name: 'Ayesha', salary: 120000, gender: 'female' },
-    { id: 4, name: 'Naduni', salary: 120000, gender: 'female' },
-  ];
   ngOnInit() {}
   ngAfterViewInit() {
     const signUpButton = document.getElementById('signUp');
